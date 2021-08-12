c691b0fd86d80a99e820e5cdf991f4007f5d5b6a
Hodicq William <hodicq.william@gmail.com>
2021-08-12 19:26:28 +0200

    home in progress, get post from db

--- FrontEnd/src/pages/Home/Home.js
+++ FrontEnd/src/pages/Home/Home.js
@@ -1,17 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.scss";
+import Axios from "axios";
 
 function Home() {
+  const [post, setPost] = useState([]);
+
   useEffect(() => {
     if (!localStorage.getItem("loggedIn")) {
       localStorage.setItem("loggedIn", false);
     }
+    if (localStorage.getItem("loggedIn",true)) {
+      Axios.get("http://localhost:3001/post").then((response) => {
+        setPost(response.data);
+      });
+    }
   }, []);
+
   return (
     <div className="home">
-      <div className="post"> 
-
-      </div>
+      {post.map((val) => {
+        return (
+          <div className="post">
+            <div className="image">
+              <img src={`http://localhost:3001/${val.image}`} alt=""/>
+            </div>
+            <div className="content">
+              <div className="title">{val.title} /by @ {val.author}</div>
+              <div className="description">{val.description}</div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
